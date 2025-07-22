d03e1be62bfc683eb277e0c81afe8d50123dbfb5
RyanStoffel <772612@calbaptist.edu>
2025-07-21 23:26:00 -0700

    Added some chages to flake.nix.

--- flake.nix
+++ flake.nix
@@ -23,6 +23,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "hm-bak";
             home-manager.users.rstoffel = import ./home.nix;
           }
         ];
